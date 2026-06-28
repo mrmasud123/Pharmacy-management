5a47280ccb170ac928acf109cbc614ea13b76f00
masud <mrmasud151821@gmail.com>
2026-06-28 11:23:14 +0600

    Migration file modified

--- database/migrations/2026_05_10_175428_create_employees_table.php
+++ database/migrations/2026_05_10_175428_create_employees_table.php
@@ -7,7 +7,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
 
             $table->id();
 
