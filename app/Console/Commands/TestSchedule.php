08d3c853dca35b30498de112975b51ef33cd73e0
masud <mrmasud151821@gmail.com>
2026-06-30 14:34:42 +0600

    Logo and test scheduler added

---
+++ app/Console/Commands/TestSchedule.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class TestSchedule extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-schedule';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Log::info("Test Scheduler runs at " . date('Y-m-d H:i:s'));
+        $this->info("Scheduler is working!");
+    }
+}
